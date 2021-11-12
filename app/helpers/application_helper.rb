ab4d7a73672f00c9506fa0b2ab56b2792d46336b
Mikey Clarke <>
2021-11-13 12:21:41 +1300

    Added placeholder images
    Miiiiight have to scrap the entire current card system in favour of background-image css, time will tell

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
 
   def first_img_src_for(post)
-    Nokogiri::HTML(post.content_cache).css('img').first['src']
+    img = Nokogiri::HTML(post.content_cache).css('img')
+    if img.length > 0
+      img.first['src']
+    else
+      'https://via.placeholder.com/300'
+    end
   end
 end
