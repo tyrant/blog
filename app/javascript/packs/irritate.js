9fa110da4519060db2bf6a03e0f0b0954cefd603
Mikey Clarke <epicschnozz@gmail.com>
2022-01-01 22:10:47 +1300

    Irritate IF bug fixed

--- app/javascript/packs/irritate.js
+++ app/javascript/packs/irritate.js
@@ -1,10 +1,9 @@
 
-
 // 2% of the time, the Irritate modal will appear 20-30 seconds after page load -
 // unless ?irritate=bringiton
 export function loadIrritateModal() {
   var urlParams = new URLSearchParams(window.location.search);
-  if (urlParams.get('irritate') !== 'bringiton' || Math.random() > 0.98)
+  if (!(urlParams.get('irritate') == 'bringiton' || Math.random() > 0.98))
     return;
 
   setTimeout(function() { 
