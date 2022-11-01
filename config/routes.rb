0950d3154bae239aa9464409d7fe83f3aec84228
Mikey Clarke <pineutrino@gmail.com>
2022-11-01 19:54:19 +1300

    Removed twin duelling route endpoints
    It'd honestly never occurred to me that we have both :blog_admin and :cms_admin both pointing at '/admin'. Bah. I'd not really had cause to look closely, and until now, CMS and ComfyBlog had been just peachy. But. The Ruby 3.1.2 upgrade, for whatever dang reason, has now started complaining. Running RSpec squirts forth a 'ArgumentError: wrong number of arguments (given 1, expected 0)` from routes.rb:5. No idea why that error in particular. But removing this rogue dupe URL seems to clear things up nicely.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root to: 'application#index'
   comfy_route :blog_admin, path: "/admin"
   comfy_route :blog, path: "/blog"
-  comfy_route :cms_admin, path: "/admin"
   # Ensure that this route is defined last
   comfy_route :cms, path: "/"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
