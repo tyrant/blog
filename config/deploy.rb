73b132590b57fa8bf13e107ecb79e0014a5460fa
Mikey Clarke <pineutrino@gmail.com>
2022-11-24 13:33:22 +1300

    Oops, typo in creds key file path

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,8 +44,8 @@ namespace :deploy do
   namespace :check do
     before :linked_files, :set_production_key do
       on roles(:app), in: :sequence, wait: 10 do
-        unless test("[ -f #{shared_path}/config/production.key ]")
-          upload! 'config/production.key', "#{shared_path}/config/production.key"
+        unless test("[ -f #{shared_path}/config/credentials/production.key ]")
+          upload! 'config/credentials/production.key', "#{shared_path}/config/credentials/production.key"
         end
       end
     end
