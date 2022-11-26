ffbb52d9ce35684a7fa1e6dfe70961026ed1bc48
Mikey Clarke <pineutrino@gmail.com>
2022-11-26 20:17:00 +1300

    Does 'cap deploy' assets:precompile need a master key too?

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,6 +46,9 @@ namespace :deploy do
   namespace :check do
     before :linked_files, :set_production_key do
       on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/config/master.key ]")
+          upload! 'config/master.key', "#{shared_path}/config/master.key"
+        end
         unless test("[ -f #{shared_path}/config/credentials/production.key ]")
           upload! 'config/credentials/production.key', "#{shared_path}/config/credentials/production.key"
         end
