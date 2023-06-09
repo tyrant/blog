72b69c8fd93b7cc295f3198f8120094e94b485e2
Mikey Clarke <pineutrino@gmail.com>
2023-06-10 11:05:36 +1200

    Baffling fixes baffle me: rebuilt admin routes
    Beyond baffling :O All of a sudden, the Admin site disappeared. I could not log in. For the life of me I could swear I'd not changed a goddamn thing in any routes. No idea why it suddenly stopped working.
    
    Anyhoo. I poked further, through the source codes of both the comfortable-mexican-sofa and comfy_blog gems ... and turns out most of the vanilla CMS stuff doesn't seem to be called directly from the comfy_blog code. That's all well and good, except for the fact that comfy_blog installation requires specifically removing the cms_admin routes from routes.rb. I have no idea how this had worked before.
    
    So I tried to re-add it, and hit Bug Two. Looks like there's some old-style-vs-new-style ruby method params bug in calling comfy_route_cms_admin ... cms/routing.rb calls its :cms and :cms_admin routing generators via send(''), dynamically. Bah. I just called comfy_route_cms_admin directly from my routes file ... and all is gold again. Aah. Why did this happen in the first place? Why break now?

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   get 'posts/:id/prev_nek', to: 'posts#prev_nek', as: 'prev_nek'
   root to: 'application#index'
 
-  comfy_route :blog_admin, path: "/admin"
-  comfy_route :blog, path: "/blog"
+  comfy_route :blog_admin, path: 'admin'
+  comfy_route :blog, path: "blog"
+
   # Ensure that this route is defined last
-  #comfy_route :cms_admin, path: '/admin'
+  #comfy_route :cms_admin, path: 'admin'
+  comfy_route_cms_admin
   comfy_route :cms, path: "/"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
