9a4f0f63eeef22ab8e3be8b6e53733bbb8aa7b3f
Mikey Clarke <pineutrino@gmail.com>
2024-11-16 15:34:28 +1300

    Added Prod email credentials
    Tested these on dev. Let's push to Prod and see if they work there too.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,10 +69,24 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
 
   Rails.application.routes.default_url_options[:host] = host
-  
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'mikeyclarke.co.nz',
+    user_name: Rails.application.credentials.dig(:smtp, :user_name),
+    password: Rails.application.credentials.dig(:smtp, :password),
+    authentication: 'plain',
+    enable_starttls: true,
+    open_timeout: 5,
+    read_timeout: 5
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
