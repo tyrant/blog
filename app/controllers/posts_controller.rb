b4bdeb30172a995bd03ee19609cb038f2e45ff6a
Mikey Clarke <pineutrino@gmail.com>
2024-11-23 17:37:44 +1300

    OtherBooks footer created and added site-wide

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < Comfy::Blog::PostsController
 
   def index
+    @show_other_books = true
+    
     scope =
       if params[:year]
         scope = @cms_site.blog_posts.published.for_year(params[:year])
