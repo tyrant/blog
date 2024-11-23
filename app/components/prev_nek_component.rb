25371403f9c49f644cb4761429beca5c8020b6e7
Mikey Clarke <pineutrino@gmail.com>
2024-11-23 19:11:14 +1300

    Prettied up prev/nek

--- app/components/prev_nek_component.rb
+++ app/components/prev_nek_component.rb
@@ -17,18 +17,34 @@ class PrevNekComponent < ViewComponent::Base
     @post.nek category: @category, nsfw: !@nsfw_options['banish']
   end
 
+  def prev_path
+    if prev.present?
+      comfy_blog_post_path(@site.path, prev.year, prev.month, prev.slug)
+    else
+      ''
+    end
+  end
+
+  def nek_path
+    if nek.present?
+      comfy_blog_post_path(@site.path, nek.year, nek.month, nek.slug)
+    else
+      ''
+    end
+  end
+
   def prev_css_classes
     classes = common_css_classes
-    classes += %w(rounded-t-lg sm:rounded-l-lg border-b-none sm:border-r-none text-right)
-    classes += nsfw_css_classes if prev.nsfw?
+    classes += %w(rounded-t-lg sm:rounded-l-lg sm:rounded-r-none border-b-none sm:border-r-none text-right)
+    classes += nsfw_css_classes if prev&.nsfw?
 
     classes.join ' '
   end
 
   def nek_css_classes
     classes = common_css_classes
-    classes += %w(rounded-b-lg sm:rounded-r-lg border-t-none sm:border-l-none)
-    classes += nsfw_css_classes if nek.nsfw?
+    classes += %w(rounded-b-lg sm:rounded-r-lg sm:rounded-l-none border-t-none sm:border-l-none)
+    classes += nsfw_css_classes if nek&.nsfw?
 
     classes.join ' '
   end
@@ -36,7 +52,7 @@ class PrevNekComponent < ViewComponent::Base
   def css_classes_for_category
     label = @category.present? ? @category.label.parameterize : 'all-posts'
 
-    classes = %w(text-center text-lg px-4 sm:px-1 py-1 h-8 -my-4 sm:h-auto sm:my-auto w-auto sm:w-20 mx-auto sm:-mx-10 md:w-24 md:-mx-12 -my-0 z-10 font-bold leading-tight shadow-lg)
+    classes = %w(text-center text-2xl px-4 sm:px-1 py-2 h-9 -my-4 sm:h-auto sm:my-auto w-auto sm:w-26 mx-auto sm:-mx-13 md:w-24 md:-mx-12 -my-0 z-10 leading-[1.3rem] shadow-lg font-['Racing_Sans_One'])
     classes << PostComponent::CAT_COMMON_CSS
     classes << PostComponent::CAT_UNIQUE_CSS[label]
     
