f603a0e4191d47c8a18b1a2489ea313734b376a1
Mikey Clarke <pineutrino@gmail.com>
2025-10-24 16:34:11 +1300

    Latest selenium-webdriver syntax
    Turns out `capabilities:` is deprecated.
    
    Lovingly copypasta'd from comedy-gigs-app.

--- spec/support/chrome_headless.rb
+++ spec/support/chrome_headless.rb
@@ -1,11 +1,15 @@
 RSpec.configure do |config|
 
   Capybara.register_driver :robspierre do |driver|
-    Capybara::Selenium::Driver.new driver,
-      browser: :chrome,
-      capabilities: [Selenium::WebDriver::Chrome::Options.new(
-        #args: %w[headless] # All available args: https://peter.sh/experiments/chromium-command-line-switches/
-      )]
+
+    options = Selenium::WebDriver::Chrome::Options.new
+    # options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1400,1400')
+
+    Capybara::Selenium::Driver.new(driver, browser: :chrome, options: options)
   end
 
   config.before(:each, type: :system) { driven_by :robspierre }
